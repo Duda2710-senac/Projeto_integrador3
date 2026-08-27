1072a66c05e4cb1542dcaeb42a68228ddd85e8d4
ARIANY1234 <1115609@senacminas.edu.br>
2026-08-27 19:17:41 -0300

    cadastro

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
 const SUPABASE_URL = "https://ecxpqfjhgmmrlkwvfwcb.supabase.co";
-
+ 
 const SUPABASE_PUBLISHABLE_KEY =
     "sb_publishable__kEK59Xeoo9jxoMgHNY3Kw_1r2D40rg";
-
+ 
 const clienteSupabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
 );
+ 
+ 
+ async function testarConexao() {
 
+    const { data, error } = await clienteSupabase
+        .from("login")
+        .select("*")
+        .limit(1);
 
+    if (error) {
 
+        console.error(error);
 
+        document.getElementById("mensagem").textContent =
+            "Erro ao conectar com o Supabase.";
 
+        return;
+    }
 
+    console.log(data);
+
+    document.getElementById("mensagem").textContent =
+        "Conexão com o Supabase funcionando!";
+}
+ 
+ 
+ 
 const loginForm =
     document.getElementById("loginForm");
-
+ 
 if (loginForm) {
-
+ 
     loginForm.addEventListener(
         "submit",
         async function(event) {
-
+ 
             event.preventDefault();
-
+ 
             const email =
                 document.getElementById("email").value;
-
+ 
             const senha =
                 document.getElementById("senha").value;
-
+ 
             const { data, error } =
                 await clienteSupabase.auth.signInWithPassword({
                     email: email,
                     password: senha
                 });
-
+ 
             if (error) {
-
+ 
                 console.error(error);
-
+ 
                 document.getElementById("mensagem").textContent =
                     "E-mail ou senha incorretos.";
-
+ 
                 return;
             }
-
+ 
             console.log(data);
-
+ 
             window.location.href =
                 "painel.html";
         }
     );
 }
-
+ 
+ 
+ 
