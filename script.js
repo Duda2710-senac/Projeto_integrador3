0b17a51c8d97c66f55261b18be8492a2f7bd5ff4
ARIANY1234 <1115609@senacminas.edu.br>
2026-08-27 19:28:09 -0300

    cadastro

--- script.js
+++ script.js
@@ -7,4 +7,52 @@ const clienteSupabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
 );
-
+ 
+ 
+ 
+    
+ 
+ 
+const loginForm =
+    document.getElementById("loginForm");
+ 
+if (loginForm) {
+ 
+    loginForm.addEventListener(
+        "submit",
+        async function(event) {
+ 
+            event.preventDefault();
+ 
+            const email =
+                document.getElementById("email").value;
+ 
+            const senha =
+                document.getElementById("senha").value;
+ 
+            const { data, error } =
+                await clienteSupabase.auth.signInWithPassword({
+                    email: email,
+                    password: senha
+                });
+ 
+            if (error) {
+ 
+                console.error(error);
+ 
+                document.getElementById("mensagem").textContent =
+                    "E-mail ou senha incorretos.";
+ 
+                return;
+            }
+ 
+            console.log(data);
+ 
+            window.location.href =
+                "painel.html";
+        }
+    );
+}
+ 
+ 
+ 
